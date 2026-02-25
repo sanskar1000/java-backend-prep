e1610d91c0a8c6a2d83c801bf639197d346b7da4
Aradhya Singh <aradhy122@gmail.com>
2026-02-25 23:12:55 +0530

    Add first string program: reverse a string

---
+++ strings/basic-programs/ReverseString.java
@@ -0,0 +1,35 @@
+/*
+Title: Reverse a String
+Topic: Strings
+Level: Beginner
+Author: Aradhya Thakur
+Date: 2026
+
+Problem Statement:
+Write a Java program to reverse a given string.
+
+Example:
+Input  : Java
+Output : avaJ
+
+Approach:
+Start from last character and append to new string.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        String str = "Java";
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + reversed);
+    }
+}
