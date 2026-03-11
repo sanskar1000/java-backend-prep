e4a98d6e1db9b0cd853d38d8e99f177707414d50
Aradhya Singh <aradhy122@gmail.com>
2026-03-11 11:59:25 +0530

    In HelloWorld program add documentation for Java basics

--- basics/HelloWorld.java
+++ basics/HelloWorld.java
@@ -1,7 +1,34 @@
 package basics;
 
+/**
+ * ------------------------------------------------------------
+ * Program Name : HelloWorld
+ * Topic        : Java Basics
+ * Level        : Beginner
+ * Author       : Aradhya Thakur
+ * Year         : 2026
+ *
+ * Description:
+ * This program demonstrates the basic structure of a Java
+ * application and prints a message to the console.
+ *
+ * Key Concept:
+ * • Every Java program starts execution from the main() method
+ * • System.out.println() is used to display output in console
+ * • A class is the basic building block of Java programs
+ *
+ * Example Output:
+ * Hello, World
+ *
+ * Time Complexity  : O(1)
+ * Space Complexity : O(1)
+ * ------------------------------------------------------------
+ */
+
 public class HelloWorld {
+
     public static void main(String[] args) {
         System.out.println("Hello, World");
     }
+
 }
