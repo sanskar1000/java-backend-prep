2b733d63c85e5d338c199d878d3f4d23ea0e91de
Aradhya Singh <aradhy122@gmail.com>
2026-04-09 21:58:47 +0530

    feat: implement sum of digits using while loop with clean documentation

--- loops/practice/SumOfDigits.java
+++ loops/practice/SumOfDigits.java
@@ -1,19 +1,58 @@
-//Practice: sum of all digit
 package loops.practice;
+
+import java.util.Scanner;
+
+/**
+ * ------------------------------------------------------------
+ * Program Name : SumOfDigits
+ * Topic        : Loops (while loop)
+ * Level        : Beginner
+ * Author       : Aradhya Thakur
+ * Year         : 2026
+ *
+ * Description:
+ * This program calculates the sum of digits of a given number.
+ *
+ * Logic:
+ * • Extract each digit using % 10
+ * • Add it to sum
+ * • Remove digit using / 10
+ *
+ * Example:
+ * Input  : 1234
+ * Output : 10
+ *
+ * Time Complexity  : O(log₁₀ n)
+ * Space Complexity : O(1)
+ * ------------------------------------------------------------
+ */
 
 public class SumOfDigits {
 
     public static void main(String[] args) {
 
-        int number = 12345;
-        int sum = 0;
+        Scanner sc = new Scanner(System.in);
 
-        while (number > 0) {
-            int digit = number % 10;   // extract last digit
-            sum = sum + digit;
-            number = number / 10;      // remove last digit
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("❌ Invalid number");
+            sc.close();
+            return;
         }
 
-        System.out.println("Sum of digits = " + sum);
+        int temp = n;
+        int sum = 0;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += digit;
+            temp = temp / 10;
+        }
+
+        System.out.println("✔ Sum of digits of " + n + " = " + sum);
+
+        sc.close();
     }
 }
