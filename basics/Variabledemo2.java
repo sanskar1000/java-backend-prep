3d08021bc0b50f8e428d8f62553967213ae93168
Aradhya Singh <aradhy122@gmail.com>
2026-06-02 23:31:20 +0530

    Refactor class name and add program documentation
    Updated the class name to 'Main' and added a detailed program description.

--- basics/Variabledemo2.java
+++ basics/Variabledemo2.java
@@ -1,11 +1,32 @@
 package basics;
 
+/**
+ * ------------------------------------------------------------
+ * Program Name : StringOutputDemo
+ * Topic        : String Variables and Escape Characters
+ * Author       : Aradhya Thakur
+ * Year         : 2026
+ *
+ * Description:
+ * Demonstrates:
+ * - String variable declaration
+ * - String initialization
+ * - Escape characters
+ * - Output formatting
+ * - Case sensitivity in Java
+ * ------------------------------------------------------------
+ */
+
 public class Main {
+
     public static void main(String[] args) {
-        // Type your code below
-        String result = "Hii there";
-        
-        // Don't change the line below
-        System.out.println("Massage  = \"" + Result + "\"");
+
+        // Declare and initialize string variable
+        String message = "Hii there";
+
+        // Display formatted output
+        System.out.println(
+                "Message = \"" + message + "\""
+        );
     }
 }
