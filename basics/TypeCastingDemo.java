0dd6a5197072010bc9161af4a6e22413445084b4
Aradhya Singh <aradhy122@gmail.com>
2026-06-02 23:28:37 +0530

    Add documentation for TypeCastingDemo program
    Added a detailed comment block explaining the program's purpose and concepts related to type casting in Java.

--- basics/TypeCastingDemo.java
+++ basics/TypeCastingDemo.java
@@ -1,9 +1,37 @@
 package basics;
 
+/**
+ * ------------------------------------------------------------
+ * Program Name : TypeCastingDemo
+ * Topic        : Type Casting in Java
+ * Level        : Beginner
+ * Author       : Aradhya Thakur
+ * Year         : 2026
+ *
+ * Description:
+ * This program demonstrates implicit type casting
+ * (widening conversion) in Java.
+ *
+ * Concepts Used:
+ * - Primitive data types
+ * - Implicit casting
+ * - Widening conversion
+ * - int to double conversion
+ * ------------------------------------------------------------
+ */
+
 class TypeCastingDemo {
+
     public static void main(String[] args) {
+
+        // Integer value
         int a = 10;
+
+        // Implicit casting (int → double)
         double b = a;
-        System.out.println(b);
+
+        System.out.println("Integer value : " + a);
+
+        System.out.println("Double value  : " + b);
     }
 }
