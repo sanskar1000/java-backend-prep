664b5f321f4ddd2cd845be56f7354d89ce5845f5
Aradhya Singh <aradhy122@gmail.com>
2026-06-03 22:39:42 +0530

    Refactor DivisibleByFive program with comments and documentation
    Updated user prompts and added detailed program documentation.

--- condition/practice/DivisibleByFive.java
+++ condition/practice/DivisibleByFive.java
@@ -1,22 +1,64 @@
-// Practice: check number is divisible by 5 or not
+// Practice Program : Check Number Divisible by 5
+
 package condition.practice;
 
 import java.util.Scanner;
 
+/**
+ * ------------------------------------------------------------
+ * Program Name : DivisibleByFive
+ * Topic        : If-Else Condition in Java
+ * Level        : Beginner
+ * Author       : Aradhya Thakur
+ * Year         : 2026
+ *
+ * Description:
+ * This program checks whether the entered number
+ * is divisible by 5 or not.
+ *
+ * A number is divisible by 5 if:
+ *
+ * remainder when divided by 5 == 0
+ *
+ * Concepts Used:
+ * - Scanner Class
+ * - User Input
+ * - if-else condition
+ * - Modulus Operator (%)
+ * - Relational Operator
+ * - Conditional Logic
+ *
+ * Time Complexity  : O(1)
+ * Space Complexity : O(1)
+ * ------------------------------------------------------------
+ */
+
 public class DivisibleByFive {
+
     public static void main(String[] args) {
 
+        // Create Scanner object
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a Number: ");
+        // Take input from user
+        System.out.print("Enter a number : ");
         int num = sc.nextInt();
 
-        if (num%5==0){
-            System.out.println("Number is divisible by 5.");
-        }else {
-            System.out.println("Number is not divisible by 5.");
+        // Check divisibility
+        if (num % 5 == 0) {
+
+            System.out.println(
+                    num + " is divisible by 5."
+            );
+
+        } else {
+
+            System.out.println(
+                    num + " is not divisible by 5."
+            );
         }
 
-
+        // Close Scanner
+        sc.close();
     }
 }
