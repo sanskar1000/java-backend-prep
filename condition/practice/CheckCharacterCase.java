6ad1cd4b079cea3ff83633910d1cc03fff5f4653
Aradhya Singh <aradhy122@gmail.com>
2026-06-03 22:18:07 +0530

    Refactor CheckCharacterCase with comments and formatting
    Updated the CheckCharacterCase program to improve formatting and added detailed comments.

--- condition/practice/CheckCharacterCase.java
+++ condition/practice/CheckCharacterCase.java
@@ -1,22 +1,65 @@
-// Practice: check character is lowercase or uppercase 
+// Practice Program : Check Character Case
+
 package condition.practice;
 
-Import java.util.Scanner;
+import java.util.Scanner;
 
-class CheckCharacterCase {
+/**
+ * ------------------------------------------------------------
+ * Program Name : CheckCharacterCase
+ * Topic        : If-Else Condition in Java
+ * Level        : Beginner
+ * Author       : Aradhya Thakur
+ * Year         : 2026
+ *
+ * Description:
+ * This program checks whether the entered character is:
+ * - Uppercase letter
+ * - Lowercase letter
+ * - Not an alphabet character
+ *
+ * Concepts Used:
+ * - Scanner Class
+ * - User Input
+ * - if-else-if ladder
+ * - Character comparison
+ * - Logical AND operator (&&)
+ *
+ * ------------------------------------------------------------
+ */
+
+public class CheckCharacterCase {
+
     public static void main(String[] args) {
 
+        // Create Scanner object
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a character: ");
+        // Take character input
+        System.out.print("Enter a character : ");
         char ch = sc.next().charAt(0);
 
+        // Check uppercase
         if (ch >= 'A' && ch <= 'Z') {
-            System.out.println("Uppercase letter");
-        } else if (ch >= 'a' && ch <= 'z') {
-            System.out.println("Lowercase letter");
-        } else {
-            System.out.println("Not an alphabet character");
-        } 
+
+            System.out.println("Uppercase Letter");
+
+        }
+
+        // Check lowercase
+        else if (ch >= 'a' && ch <= 'z') {
+
+            System.out.println("Lowercase Letter");
+
+        }
+
+        // Not an alphabet
+        else {
+
+            System.out.println("Not an Alphabet Character");
+        }
+
+        // Close Scanner
+        sc.close();
     }
 }
