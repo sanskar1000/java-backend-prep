69480f8181529505e3dab86c37caf9463eef835b
Aradhya Singh <aradhy122@gmail.com>
2026-06-03 22:14:54 +0530

    Refactor AdultOrMinor program comments and prompts
    Updated comments and print statements for clarity.

--- condition/practice/AdultOrMinor.java
+++ condition/practice/AdultOrMinor.java
@@ -1,20 +1,55 @@
-//Practice : check person Adult or Minor
+// Practice Program : Check Person is Adult or Minor
+
 package condition.practice;
 
 import java.util.Scanner;
 
-public class AdultOrMinor  {
+/**
+ * ------------------------------------------------------------
+ * Program Name : AdultOrMinor
+ * Topic        : If-Else Condition in Java
+ * Level        : Beginner
+ * Author       : Aradhya Thakur
+ * Year         : 2026
+ *
+ * Description:
+ * This program checks whether a person is:
+ * - Adult
+ * - Minor
+ *
+ * based on the entered age.
+ *
+ * Concepts Used:
+ * - User Input
+ * - Scanner Class
+ * - if-else condition
+ * - Relational Operators
+ *
+ * ------------------------------------------------------------
+ */
+
+public class AdultOrMinor {
+
     public static void main(String[] args) {
 
+        // Create Scanner object
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a person age : ");
+        // Take age input
+        System.out.print("Enter person's age : ");
         int age = sc.nextInt();
 
-        if ( age>=18){
+        // Check condition
+        if (age >= 18) {
+
             System.out.println("Person is Adult");
-        }else {
+
+        } else {
+
             System.out.println("Person is Minor");
         }
-        }
+
+        // Close Scanner
+        sc.close();
     }
+}
