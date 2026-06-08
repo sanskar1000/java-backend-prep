3be1979f0bffa8b8288a18d8a4a26dc3e7676191
Aradhya Singh <aradhy122@gmail.com>
2026-06-08 22:36:51 +0530

    Refactor comments and format greet method
    Updated comments and formatting in MethodBasics.java.

--- methods/MethodBasics.java
+++ methods/MethodBasics.java
@@ -1,18 +1,31 @@
 package methods;
 
-/*
- Program: MethodBasics
- Purpose: Demonstrate basic method creation and usage
- Concept: Method definition
-*/
+/**
+ * ------------------------------------------------------------
+ * Program Name : MethodBasics
+ * Topic        : Java Methods
+ * Description:
+ * Demonstrates:
+ * - method definition
+ * - method calling
+ * - code reusability
+ * ------------------------------------------------------------
+ */
 
 public class MethodBasics {
 
+    /**
+     * Displays a welcome message.
+     */
     static void greet() {
-        System.out.println("Hello, welcome to Java methods!");
+
+        System.out.println(
+                "Hello, welcome to Java methods!"
+        );
     }
 
     public static void main(String[] args) {
-        greet();   // method call
+
+        greet();
     }
 }
