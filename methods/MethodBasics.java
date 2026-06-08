6025e3578431ac5cf2c6bae2430bb076ab263bf0
Aradhya Singh <aradhy122@gmail.com>
2026-06-08 22:37:21 +0530

    Enhance MethodBasics documentation and greeting
    Updated program documentation and added a greeting message.

--- methods/MethodBasics.java
+++ methods/MethodBasics.java
@@ -4,11 +4,26 @@ package methods;
  * ------------------------------------------------------------
  * Program Name : MethodBasics
  * Topic        : Java Methods
+ * Level        : Beginner
+ * Author       : Aradhya Thakur
+ * Year         : 2026
+ *
  * Description:
- * Demonstrates:
+ * This program demonstrates:
  * - method definition
  * - method calling
+ * - static methods
  * - code reusability
+ * - program execution flow
+ *
+ * Concepts Used:
+ * - Classes and Objects
+ * - Methods
+ * - Method Invocation
+ * - Static Keyword
+ *
+ * Time Complexity  : O(1)
+ * Space Complexity : O(1)
  * ------------------------------------------------------------
  */
 
@@ -25,6 +40,10 @@ public class MethodBasics {
     }
 
     public static void main(String[] args) {
+
+        System.out.println(
+                "----- Method Demonstration -----"
+        );
 
         greet();
     }
