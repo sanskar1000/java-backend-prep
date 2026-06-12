1c4d3a3c4cb903f2da88628575758e7377bfe1ec
Aradhya Singh <aradhy122@gmail.com>
2026-06-12 22:20:54 +0530

    Refactor PrimeCheckMethod and add documentation
    Updated the isPrime method to improve efficiency and added documentation.

--- methods/practice/PrimeCheckMethod.java
+++ methods/practice/PrimeCheckMethod.java
@@ -1,20 +1,77 @@
-// Practice : Check number is prime or not 
 package methods.practice;
+
+/**
+ * ------------------------------------------------------------
+ * Program Name : PrimeCheckMethod
+ * Topic        : Methods in Java
+ * Level        : Beginner
+ * Author       : Aradhya Thakur
+ * Year         : 2026
+ *
+ * Description:
+ * This program checks whether a given number
+ * is prime or not using a method.
+ *
+ * A prime number is divisible only by:
+ * - 1
+ * - itself
+ *
+ * Examples:
+ * 2, 3, 5, 7, 11, 13 ...
+ *
+ * Concepts Used:
+ * - Methods
+ * - Boolean Return Type
+ * - Loops
+ * - Conditional Statements
+ * - Mathematical Optimization
+ *
+ * Time Complexity  : O(√n)
+ * Space Complexity : O(1)
+ * ------------------------------------------------------------
+ */
 
 public class PrimeCheckMethod {
 
-     static boolean isPrime(int n) {
-        if (n <= 1) return false;
+    /**
+     * Checks whether a number is prime.
+     *
+     * @param number number to check
+     * @return true if prime, otherwise false
+     */
+    static boolean isPrime(int number) {
 
-        for (int i = 2; i <= n / 2; i++) {
-            if (n % i == 0) {
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int divisor = 2;
+             divisor * divisor <= number;
+             divisor++) {
+
+            if (number % divisor == 0) {
                 return false;
             }
         }
+
         return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(isPrime(11));
+
+        int number = 11;
+
+        if (isPrime(number)) {
+
+            System.out.println(
+                    number + " is a prime number."
+            );
+
+        } else {
+
+            System.out.println(
+                    number + " is not a prime number."
+            );
+        }
     }
 }
