bab8f4dd3addc1d832aa594f9c4501353f7fec40
Aradhya Singh <aradhy122@gmail.com>
2026-06-16 23:56:44 +0530

    Refactor variable names and update print statements and documentation

--- operators/RelationalOperatorsDemo.java
+++ operators/RelationalOperatorsDemo.java
@@ -2,24 +2,70 @@ package operators;
 
 import java.util.Scanner;
 
+/**
+ * ------------------------------------------------------------
+ * Program Name : RelationalOperatorsDemo
+ * Topic        : Relational Operators in Java
+ * Level        : Beginner
+ * Author       : Aradhya Thakur
+ * Year         : 2026
+ *
+ * Description:
+ * This program demonstrates the use of
+ * relational operators in Java.
+ *
+ * Operators Covered:
+ * >   Greater Than
+ * <   Less Than
+ * >=  Greater Than or Equal To
+ * <=  Less Than or Equal To
+ * ==  Equal To
+ * !=  Not Equal To
+ *
+ * Concepts Used:
+ * - Variables
+ * - User Input
+ * - Relational Operators
+ * - Boolean Values
+ *
+ * Time Complexity  : O(1)
+ * Space Complexity : O(1)
+ * ------------------------------------------------------------
+ */
+
 public class RelationalOperatorsDemo {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter first number: ");
-        int a = sc.nextInt();
+        System.out.print("Enter first number : ");
+        int firstNumber = sc.nextInt();
 
-        System.out.print("Enter second number: ");
-        int b = sc.nextInt();
+        System.out.print("Enter second number : ");
+        int secondNumber = sc.nextInt();
 
-        System.out.println("\n--- Relational Operators ---");
-        System.out.println("a > b  : " + (a > b));
-        System.out.println("a < b  : " + (a < b));
-        System.out.println("a >= b : " + (a >= b));
-        System.out.println("a <= b : " + (a <= b));
-        System.out.println("a == b : " + (a == b));
-        System.out.println("a != b : " + (a != b));
+        System.out.println();
+        System.out.println("----- Relational Operators -----");
+
+        System.out.println("firstNumber > secondNumber  : "
+                + (firstNumber > secondNumber));
+
+        System.out.println("firstNumber < secondNumber  : "
+                + (firstNumber < secondNumber));
+
+        System.out.println("firstNumber >= secondNumber : "
+                + (firstNumber >= secondNumber));
+
+        System.out.println("firstNumber <= secondNumber : "
+                + (firstNumber <= secondNumber));
+
+        System.out.println("firstNumber == secondNumber : "
+                + (firstNumber == secondNumber));
+
+        System.out.println("firstNumber != secondNumber : "
+                + (firstNumber != secondNumber));
+
+        sc.close();
     }
 }
