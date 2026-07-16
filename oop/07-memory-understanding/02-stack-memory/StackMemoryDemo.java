eefb008aae2979a8ce61f4c30e11248a93782209
Aradhya Singh <aradhy122@gmail.com>
2026-07-16 15:08:06 +0530

    Add StackMemoryDemo class to demonstrate stack memory

---
+++ oop/07-memory-understanding/02-stack-memory/StackMemoryDemo.java
@@ -0,0 +1,16 @@
+public class StackMemoryDemo {
+
+    static void display() {
+
+        int number = 10;
+
+        System.out.println("Number = " + number);
+    }
+
+    public static void main(String[] args) {
+
+        display();
+
+        System.out.println("Back to main()");
+    }
+}
