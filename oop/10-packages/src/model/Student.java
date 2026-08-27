240ea8e8f1ab0a3bd9da973a706dac931238236d
Aradhya Singh <aradhy122@gmail.com>
2026-08-27 21:55:21 +0530

    Add Student class to model package

---
+++ oop/10-packages/src/model/Student.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * Represents a student.
+ *
+ * <p>This class is placed inside the {@code model} package
+ * to demonstrate how packages organize related classes.</p>
+ */
+public class Student {
+
+    /**
+     * Represents the study behavior of a student.
+     */
+    public void study() {
+        System.out.println("Student is studying.");
+    }
+}
