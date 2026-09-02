56d711c0a156556234f73bac6e5bdaa64d95cf71
Aradhya Singh <aradhy122@gmail.com>
2026-09-02 15:38:13 +0530

    Add NormalFlow.java to demonstrate normal execution

---
+++ exception-handling/01-why-exceptions-exist/examples/NormalFlow.java
@@ -0,0 +1,11 @@
+public class NormalFlow {
+
+    public static void main(String[] args) {
+
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+
+        System.out.println("Program continues...");
+    }
+}
