9770263a1102f979f7437a0e91d0b2d11f9bea46
Aradhya Singh <aradhy122@gmail.com>
2026-09-02 15:38:54 +0530

    Create ArithmeticExceptionDemo class
    Added a demo for handling ArithmeticException in Java.

--- exception-handling/01-why-exceptions-exist/examples/ArithmeticExceptionDemo.java
+++ exception-handling/01-why-exceptions-exist/examples/ArithmeticExceptionDemo.java
@@ -1,1 +1,12 @@
+public class ArithmeticExceptionDemo {
 
+    public static void main(String[] args) {
+
+        System.out.println("Start");
+
+        int result = 10 / 0;
+
+        System.out.println(result);
+        System.out.println("End");
+    }
+}
